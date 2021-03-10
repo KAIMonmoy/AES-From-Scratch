a96840f3c902d274d4294eb3e37392a9ca96ad35
KAIMonmoy <aftarulislam@gmail.com>
2021-03-11 01:32:27 +0600

    :construction: Scaffolded AES class

--- AES.py
+++ AES.py
@@ -1,5 +1,19 @@
 import numpy as np
 import BitVector
 
+
+class AES:
+
+    def __init__(self, key: str) -> None:
+        super().__init__()
+        self.key_plaintext = key
+
+    def encrypt(self, plaintext):
+        pass
+
+    def decrypt(self, cyphertext):
+        pass
+
+
 if __name__ == '__main__':
     print('Hello, AES!')
